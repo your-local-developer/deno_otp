38744f2121f17641421dbec733663af84f97511e
Jan <your-local-developer@git.slmail.me>
2022-07-22 15:26:56 +0200

    Remove(deps): Remove Buffer

--- deps.ts
+++ deps.ts
@@ -2,5 +2,3 @@ export {
   byteLength,
   decode,
 } from "https://deno.land/std@0.146.0/encoding/base32.ts";
-// TODO: Remove this dependency to make this module browser compatible
-export { Buffer } from "https://deno.land/std@0.146.0/node/buffer.ts";
