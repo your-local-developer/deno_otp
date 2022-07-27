0c24a3cd3cc08ff63887b50050def2df064f7818
Jan <your-local-developer@git.slmail.me>
2022-07-27 15:43:06 +0200

    Move encode to deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 export {
   byteLength,
   decode,
+  encode,
 } from "https://deno.land/std@0.146.0/encoding/base32.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,1 @@
 export * from "https://deno.land/std@0.146.0/testing/asserts.ts";
-export { encode } from "https://deno.land/std@0.146.0/encoding/base32.ts";
